1509eb73e4e7d05a787f60be7da542687f7b3068
RIZZIALPADISTA <161547478+RIZZIALPADISTA@users.noreply.github.com>
2024-06-02 12:57:17 +0700

    Add files via upload

---
+++ b.py
@@ -0,0 +1,5 @@
+print('Hello World! A')
+print('Hello World! B')
+print('Hello World! C')
+print('Hello World! D')
+print('Hello World! E')
---
+++ c.js
@@ -0,0 +1,13 @@
+console.log('Hello World! A')
+console.log('Hello World! B')
+console.log('Hello World! C')
+console.log('Hello World! D')
+console.log('Hello World! E')
+
+let x = {
+    'nama': 'Ali',
+    'usia': 20,
+    'kota': 'Jakarta'
+}
+console.log(x)
+console.log(typeof(x))
